bc865baace18c926995d3cc66bdfd61f8062d8b1
marduk-tendo <tenshiienoko51@gmail.com>
2023-12-28 14:35:57 +0100

    add files

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// int var1 = 10;
+// int var2 = 100;
+// float var3 = 0.0;
+
+int main(){
+    // printf("\t'On effectue une tabulation en debut de texte'\n");
+    // printf("\nOn effectue un retour à la ligne avant d'afficher cette ligne\n\n");
+    // printf("\tOn effectue une tabulation en debut de texte\n");
+    
+    printf("bonjour%cà%ctoi",9 ,9);
+}
